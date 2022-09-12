be71d98a34cc1a7e2f464e826dd7592f71cb9301
Shea <shea@vulpesdevelopment.net>
2022-09-11 21:27:13 -0400

    initial

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use {std::sync::mpsc, tray_item::TrayItem};
+
+enum Message {
+    Quit,
+}
+
+fn main() {
+    let mut tray = TrayItem::new("Tray Example", "tray").unwrap();
+
+    tray.add_label("Tray Label").unwrap();
+
+    tray.add_menu_item("Hello", || {
+        println!("Hello!");
+    })
+    .unwrap();
+
+    let (tx, rx) = mpsc::channel();
+
+    tray.add_menu_item("Quit", move || {
+        println!("Quit");
+        tx.send(Message::Quit).unwrap();
+    })
+    .unwrap();
+
+    loop {
+        match rx.recv() {
+            Ok(Message::Quit) => break,
+            _ => {}
+        }
+    }
+}
